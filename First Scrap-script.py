4ee2f9dd4b1ef101ba7e60a71c3a7d33d2369f6b
Laurence <ceduardo.avila@alumnos.udg.mx>
2022-08-04 22:05:02 -0500

    a third change

--- First Scrap-script.py
+++ First Scrap-script.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+#>Create and link a github repo
 # Nuevo cambio al repositorio: 
-# $ git add "file"
+# $ git add "/file"
 # $ git commit -m "type a commit message"
 # $ git push -u origin main
 
