ab2a85deeb3538ead556956930f164cfa9e0f0d7
Laurence <ceduardo.avila@alumnos.udg.mx>
2022-08-06 14:23:35 -0500

    create window display mainframe

--- First Scrap-script.py
+++ First Scrap-script.py
@@ -12,9 +12,17 @@ import tkinter as tk
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.geometry("480x760")
-        self.window.resizable("0,0")
+        self.window.geometry("375x467")
+        self.window.resizable(0, 0)
         self.window.title("Quickie Calculator")
+        #self.display_frame = self.create_display_frame()
+        #self.buttoms_frame = self.create_buttoms_frame()
+    def run(self):
+        self.window.mainloop()
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
+
 print("This is a second change")
 
 
